6784ce9f7487b57a456a90c4c6659d069d13f7e7
Marc Straube <email@marcstraube.de>
2022-10-23 16:04:24 +0200

    - Update README and code for module fork. - Add configuration options for image animation style and pause text.

--- Changelog.md
+++
@@ -1,9 +0,0 @@
-# Custom Pause Foundry VTT Module Changelog
-
-## 1.0.0 Initial release
-
-- Ability to set custom pause icon for your game.
-
-## 1.0.1 Initial release
-
-- Fixed typo in module.json
--- scripts/module.js
+++ scripts/module.js
@@ -1,30 +1,87 @@
 Hooks.on("init", () => {
-    game.settings.register("custom-pause", "chooseFile", {
-        name: game.i18n.localize("CUSTOM_PAUSE.SELECT_IMAGE"),
-        hint: game.i18n.localize("CUSTOM_PAUSE.SELECT_IMAGE_HINT"),
+    game.settings.register("pause-customizer", "chooseFile", {
+        name: game.i18n.localize("PAUSE_CUSTOMIZER.SELECT_IMAGE"),
+        hint: game.i18n.localize("PAUSE_CUSTOMIZER.SELECT_IMAGE_HINT"),
         scope: "world",
         config: true,
         type: String,
         default: "icons/svg/clockwork.svg",
-        filePicker: true // This is the important part
+        filePicker: "image", // This is the important part
+        requiresReload: true
     });
+
+    game.settings.register("pause-customizer", "animationDirection", {
+		name: game.i18n.localize("PAUSE_CUSTOMIZER.ANIMATION_DIRECTION"),
+        hint: game.i18n.localize("PAUSE_CUSTOMIZER.ANIMATION_DIRECTION_HINT"),
+        scope: "world",
+        config: true,
+        type: String,
+        default: "normal",
+        choices: {
+            "normal": "normal",
+            "reverse": "reverse",
+            "alternate": "alternate",
+            "alternate-reverse": "alternate-reverse"
+        },
+        requiresReload: true
+	});
+
+    game.settings.register("pause-customizer", "animationDuration", {
+		name: game.i18n.localize("PAUSE_CUSTOMIZER.ANIMATION_DURATION"),
+        hint: game.i18n.localize("PAUSE_CUSTOMIZER.ANIMATION_DURATION_HINT"),
+        scope: "world",
+        config: true,
+        type: String,
+        default: "5s",
+        requiresReload: true
+	});
+
+    game.settings.register("pause-customizer", "animationTiming", {
+		name: game.i18n.localize("PAUSE_CUSTOMIZER.ANIMATION_TIMING"),
+        hint: game.i18n.localize("PAUSE_CUSTOMIZER.ANIMATION_TIMING_HINT"),
+        scope: "world",
+        config: true,
+        type: String,
+        default: "linear",
+        requiresReload: true
+	});
+
+    game.settings.register("pause-customizer", "pauseText", {
+		name: game.i18n.localize("PAUSE_CUSTOMIZER.PAUSE_TEXT"),
+        hint: game.i18n.localize("PAUSE_CUSTOMIZER.PAUSE_TEXT_HINT"),
+        scope: "world",
+        config: true,
+        type: String,
+        default: "",
+        requiresReload: true
+	});
 });
 
 Hooks.on("renderPause", (app, html, options) => {
     if (options.paused) {
-      html.find("img")[0].src = (game.settings.get("custom-pause", "chooseFile"))
+        let img = html.find("img")[0];
+        img.src = game.settings.get("pause-customizer", "chooseFile");
+
+        let style = "--fa-animation-direction: " + game.settings.get("pause-customizer", "animationDirection") + ";";
+        style += "--fa-animation-duration: " + game.settings.get("pause-customizer", "animationDuration") + ";";
+        style += "--fa-animation-timing: " + game.settings.get("pause-customizer", "animationTiming") + ";";
+        img.style = style;
+
+        let caption = html.find("figcaption")[0];
+        let pauseText = game.settings.get("pause-customizer", "pauseText");
+
+        if (pauseText !== "") {
+            caption.textContent = pauseText;
+        }
     }
 });
 
 Hooks.once('libChangelogsReady', function () {
     libChangelogs.register(
-        "custom-pause",
+        "pause-customizer",
         "<ul>" +
-        "<li>Update module compatibility to FoundryVTT version 10.</li>" +
-        "<li>Add German translation.</li>" +
-        "<li>Add support for <a href='https://github.com/theripper93/libChangelogs' target='_blank' style='font-style: bold;'>Changelogs & Conflicts</a> module.</li>" +
-        "<li>Add support for <a href='https://github.com/League-of-Foundry-Developers/bug-reporter' target='_blank' style='font-style: bold;'>Bug Reporter</a> module.</li>" +
+        "<li>Initial release of <strong>Pause Customizer</strong> module.</li>" +
         "</ul>",
-        "minor"
+        "major"
     );
 });
